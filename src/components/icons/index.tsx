fa269d40c07dfd752dfefffe97819ef29e94a310
Dreamer <hoabanhanh@gmail.com>
2024-06-06 16:04:18 +0700

    style: add icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,15 @@
+import MenuBurgerIcon from './assets/menu-burger.svg?react'
+import SearchIcon from './assets/search.svg?react'
+import SendIcon from './assets/send.svg?react'
+import LaughIcon from './assets/emoji-laughing.svg?react'
+import PlusCircleIcon from './assets/plus-circle.svg?react'
+import ThreeDotVerticalIcon from './assets/three-dot-vertical.svg?react'
+
+export {
+  MenuBurgerIcon,
+  SearchIcon,
+  SendIcon,
+  LaughIcon,
+  PlusCircleIcon,
+  ThreeDotVerticalIcon,
+}
