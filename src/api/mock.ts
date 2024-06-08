6c5a82fc5ec52f41e5d5e2074887df0c96ba59de
Dreamer <hoabanhanh@gmail.com>
2024-06-08 21:49:39 +0700

    feat: basic ui message list

--- src/api/mock.ts
+++ src/api/mock.ts
@@ -3,3 +3,58 @@ export const user = {
   avatar_path:
     'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww',
 }
+
+export const channels = [
+  {
+    avatar_path:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Bill Kuphal',
+    latest_message: 'The weather will be perfect for go to camping',
+    time: '9:41 AM',
+  },
+  {
+    avatar_path:
+      'https://plus.unsplash.com/premium_photo-1658527049634-15142565537a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww',
+    name: 'Photograper',
+    latest_message: "Here's my latest drone shotsHere's my latest drone shots",
+    time: '9:16 AM',
+  },
+
+  {
+    avatar_path:
+      'https://images.unsplash.com/photo-1580489944761-15a19d654956?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D',
+    name: 'Daryl Bogisich',
+    latest_message: 'You:Store is out of stock',
+    time: 'Yesterday',
+  },
+
+  {
+    avatar_path:
+      'https://plus.unsplash.com/premium_photo-1669882305300-38b609862bee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fHww',
+    name: 'SpaceX Crew-16 Launch',
+    latest_message: "I've been there!",
+    time: 'Thursday',
+  },
+
+  {
+    avatar_path:
+      'https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww',
+    name: 'Lela Walsh',
+    latest_message: "Next time it's my turn",
+    time: '12/22/21',
+  },
+  {
+    avatar_path:
+      'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Roland Marks',
+    latest_message: '@waldo Glad to hear that 🐱',
+    time: '12/16/21',
+  },
+  {
+    avatar_path:
+      'https://images.unsplash.com/photo-1628157588553-5eeea00af15c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Helen Flatley',
+    latest_message: 'You:Ok',
+    time: '12/13/21',
+  },
+]
---
+++ src/components/channels.tsx
@@ -0,0 +1,42 @@
+import { channels } from '../api/mock'
+import { ChannelType } from '../lib/app.type'
+import UserAvatar from './user-avatar'
+
+const Channels: React.FC = () => {
+  return (
+    <div>
+      <ul className="overflow-x-hidden w-full">
+        {channels.map((channel: ChannelType) => {
+          return <Channel key={channel.name} {...channel} />
+        })}
+        {channels.map((channel: ChannelType) => {
+          return <Channel key={channel.name} {...channel} />
+        })}
+        {channels.map((channel: ChannelType) => {
+          return <Channel key={channel.name} {...channel} />
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Channels
+
+const Channel = ({ name, time, avatar_path, latest_message }: ChannelType) => {
+  return (
+    <li className="hover:bg-grey-200 dark:hover:bg-grey-800 cursor-pointer">
+      <div className="flex items-center px-2 py-3">
+        <div>
+          <UserAvatar alt={name} src={avatar_path} size="lg" />
+        </div>
+        <div className="pl-2 flex-1 pr-2 flex flex-col">
+          <span className="font-bold">{name}</span>
+          <div className="flex items-center text-grey-500">
+            <p className="text-sm  max-w-44 truncate ">{latest_message}</p>
+            <span className="ml-auto">{time}</span>
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+}
