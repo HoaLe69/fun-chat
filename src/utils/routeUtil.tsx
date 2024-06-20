3100a3e53d8b223eec11208f3679f926d8340930
Dreamer <hoabanhanh@gmail.com>
2024-06-20 22:51:29 +0700

    feat: protect route

--- src/utils/routeUtil.tsx
+++ src/utils/routeUtil.tsx
@@ -6,15 +6,16 @@ import { verifyAsync } from '../api/user.api'
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const authenticated = useAppSelector(userSelector.selectAuthenticated)
-  const user = useAppSelector(userSelector.selectUser)
-  console.log(user)
   const dispatch = useAppDispatch()
+
   useEffect(() => {
     dispatch(verifyAsync())
   }, [])
+
   if (authenticated === null) return null
   if (authenticated === false) {
     return <Navigate to={'/login'} replace={true} />
   }
+
   return children
 }
