d9168305f0610ad0d25972d6138112459c50e830
Dreamer <hoabanhanh@gmail.com>
2024-09-09 14:38:51 +0700

    style: loading logo

--- fun-chat-ui/src/components/loading.tsx
+++ fun-chat-ui/src/components/loading.tsx
@@ -2,8 +2,19 @@ import ReactLoading from 'react-loading'
 
 const LoadingIndicator = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-black/10">
-      <ReactLoading type="spin" color="#ffffff" height="3%" width="3%" />
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-5xl font-bold flex-1">
+          <strong className="text-blue-500">FUN</strong>
+          CHAT
+        </h1>
+        <ReactLoading
+          type="spinningBubbles"
+          color="#005fff"
+          height="20%"
+          width="20%"
+        />
+      </div>
     </div>
   )
 }
