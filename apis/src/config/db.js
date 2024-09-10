cbf930d5b380a44da5df80ee413cfbeaf71e715c
Dreamer <hoabanhanh@gmail.com>
2024-09-10 15:59:16 +0700

    feat: ready to production

--- apis/src/config/db.js
+++ apis/src/config/db.js
@@ -5,10 +5,11 @@ dotenv.config()
 const database = {
   host: process.env.HOST,
   name: process.env.DB_NAME,
+  mongourl: process.env.MONGODB_URL,
 }
 const connect = async () => {
   try {
-    await mongoose.connect(`${database.host}/${database.name}`)
+    await mongoose.connect(database.mongourl)
     console.log("connected database")
   } catch (e) {
     console.error("Failed to connect database")
